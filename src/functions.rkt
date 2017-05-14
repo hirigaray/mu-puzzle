5eaed2f31d71cf96b47eb27bc31d3b200895d353
Luiz de Milon <luiz.de.milon@gmail.com>
2017-05-14 02:59:17 -0400

    I already broke all the rules so might as well make it easier to read

--- src/functions.rkt
+++ src/functions.rkt
@@ -10,34 +10,34 @@
 (define (xI->xIU l)
   (cond
     [(not (equal? 'I (last l))) l]
-    [else (reverse (cons 'U (reverse l)))]))
+                          [else (reverse (cons 'U (reverse l)))]))
 
 ; returns list l with a duplicated string x after the ith instance of the M character
 (define (Mx->Mxx l i)
   ; str stores the string before the current Mx
   (let aux ((in l) (cnt i) (str '()) (out '()))
     (cond
-      [(null? in) (append (reverse str) out)]
-      [(not (zero? cnt)) (aux (cdr in) (- cnt 1) (cons (car in) str) out)]
-      [(not (equal? 'M (car in))) (aux (cdr in) cnt (cons (car in) str) out)]
-      [else (aux '() cnt str (append in (cdr in)))])))
+                   [(null? in) (append (reverse str) out)]
+            [(not (zero? cnt)) (aux (cdr in) (- cnt 1) (cons (car in) str) out)]
+   [(not (equal? 'M (car in))) (aux (cdr in) cnt       (cons (car in) str) out)]
+                         [else (aux '()      cnt       str                 (append in (cdr in)))])))
 
 ; returns list l with the ith instance of '(I I I) replaced with 'U
 (define (III->U l i)
   (let aux ((in l) (cnt i) (out '()))
     (cond
-      [(null? in) (reverse out)]
-      [(not (>= (length in) 3)) (aux (cdr in) cnt (cons (car in) out))]
-      [(not (equal? '(I I I) (take in 3))) (aux (cdr in) cnt (cons (car in) out))]
-      [(not (zero? cnt)) (aux (cdr in) (- cnt 1) (cons (car in) out))]
-      [else (aux (cdddr in) cnt (cons 'U out))])))
+                             [(null? in) (reverse out)]
+               [(not (>= (length in) 3)) (aux (cdr in) cnt       (cons (car in) out))]
+                      [(not (zero? cnt)) (aux (cdr in) (- cnt 1) (cons (car in) out))]
+    [(not (equal? '(I I I) (take in 3))) (aux (cdr in) cnt       (cons (car in) out))]
+    [else (aux (cdddr in) cnt (cons 'U out))])))
 
 ; returns list l without the ith instance of UU
 (define (dropUU l i)
   (let aux ((in l) (cnt i) (out '()))
     (cond
-      [(null? in) (reverse out)]
-      [(not (>= (length in) 2)) (aux (cdr in) cnt (cons (car in) out))]
-      [(not (equal? '(U U) (take in 2))) (aux (cdr in) cnt (cons (car in) out))]
-      [(not (zero? cnt)) (aux (cdr in) (- cnt 1) (cons (car in) out))]
-      [else (aux (cddr in) (- cnt 1) out)])))
+                             [(null? in) (reverse out)]
+               [(not (>= (length in) 2)) (aux (cdr in)  cnt       (cons (car in) out))]
+                      [(not (zero? cnt)) (aux (cdr in)  (- cnt 1) (cons (car in) out))]
+      [(not (equal? '(U U) (take in 2))) (aux (cdr in)  cnt       (cons (car in) out))]
+                                   [else (aux (cddr in) (- cnt 1) out)])))
